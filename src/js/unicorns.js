e20bbfb3181a14d4587968f81347ff4137e5be7d
Tatiana Ganzhinova <39487464+PesukarhuTG@users.noreply.github.com>
2018-08-26 20:07:25 +0300

    mart without js

---
+++ src/js/unicorns.js
@@ -0,0 +1,9 @@
+function isEven(N)
+ {if (N%2==0) return true;
+    else return false;}
+
+console.log(isEven(50));
+console.log(isEven(75));
+console.log(isEven(-1));
+console.log(isEven(0));
+console.log(isEven(1));
